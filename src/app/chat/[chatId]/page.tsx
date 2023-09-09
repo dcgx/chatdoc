2f4915d2d7cf4a3a7e489ee7b928fdc3ef262aff
Diego C/G <diego.castilloguajardo@gmail.com>
2023-09-09 02:51:49 -0300

    ✨ feat(chat): add chat sidebar

---
+++ src/app/chat/[chatId]/page.tsx
@@ -0,0 +1,43 @@
+import ChatSidebar from '@/components/ChatSidebar'
+import { db } from '@/lib/db'
+import { chats } from '@/lib/db/schema'
+import { auth } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+type Props = {
+  params: {
+    chatId: string
+  }
+}
+
+const ChatPage = async ({ params: { chatId } }: Props) => {
+  const { userId } = await auth()
+  if (!userId) {
+    return redirect('/sign-in')
+  }
+  const _chats = await db.select().from(chats).where(eq(chats.userId, userId))
+  if (!_chats) {
+    return redirect('/')
+  }
+  if (!_chats.find((chat) => chat.id === parseInt(chatId))) {
+    return redirect('/')
+  }
+
+  return (
+    <div className="flex max-h-screen overflow-scroll">
+      <div className="flex w-full max-h-screen overflow-scroll">
+        <div className="flex-[1] max-w-xs">
+          <ChatSidebar chats={_chats} chatId={parseInt(chatId)} isPro={false} />
+        </div>
+        {/* pdf viewer */}
+        <div className="max-h-screen p-4 oveflow-scroll flex-[5]"></div>
+        {/* chat component */}
+        <div className="flex-[3] border-l-4 border-l-slate-200"></div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatPage
